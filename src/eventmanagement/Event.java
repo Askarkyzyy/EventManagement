4423798606a16c8453ceacbe9bf0e5e9ba637be5
Tomiris <252451@astanait.edu.kz>
2025-12-18 22:38:23 +0500

    update Event and Participiant class structures

--- src/eventmanagement/Event.java
+++ src/eventmanagement/Event.java
@@ -6,4 +6,24 @@ public class Event {
     private String date;
     private String location;
 
+    //Constructor
+    public Event(int id,String name,String date,String location){
+        if(id>0) this.id=id; else this.id=1;
+        this.name=name;
+        this.date=date;
+        this.location=location;
+    }
+    //Getters and setters
+    public int getId(){ return id;}
+    public void setId(int id){if(id>0) this.id=id;}
+
+    public String getName() { return name; }
+    public void setName(String name) { if(name != null && !name.isEmpty()) this.name=name; }
+
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location;}
+
 }
--- src/eventmanagement/Participiant.java
+++ src/eventmanagement/Participiant.java
@@ -1,4 +1,7 @@
 package eventmanagement;
 
 public class Participiant {
+    private int id;
+    private String name;
+    private String email;
 }
