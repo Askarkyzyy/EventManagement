dcf1bd72a9ec67af8da9baaf659fcab5016eff63
Tomiris <252451@astanait.edu.kz>
2025-12-18 23:07:11 +0500

    Add addEvent and addParticipant methods to EventManager

--- src/eventmanagement/EventManager.java
+++ src/eventmanagement/EventManager.java
@@ -3,4 +3,14 @@ import java.util.ArrayList;
 public class EventManager {
     private ArrayList<Event> events;
     private ArrayList<Participiant> participants;
+
+    // Add a new event
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+
+    // Add a new participant
+    public void addParticipant(Participiant participant) {
+        participants.add(participant);
+    }
 }
